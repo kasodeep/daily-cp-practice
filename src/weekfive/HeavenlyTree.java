9c07ed0502174ced505ceb5c04df3fb098b65ab2
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-02-09 17:36:32 +0530

    Let's Go! Revenge Time!

---
+++ src/weekfive/HeavenlyTree.java
@@ -0,0 +1,37 @@
+package weekfive;
+
+import java.util.Scanner;
+
+public class HeavenlyTree {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            boolean[] check = new boolean[n + 1];
+            while (m-- > 0){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+                check[b] = true;
+            }
+
+            int common = -1;
+            for(int i = 1; i <= n; i++){
+                if(!check[i]){
+                    common = i;
+                    break;
+                }
+            }
+
+            for (int i = 1; i <= n; i++){
+                if(common == i) continue;
+                System.out.println(common + " " + i);
+            }
+        }
+    }
+}
---
+++ src/weekfive/StrongVertices.java
@@ -0,0 +1,35 @@
+package weekfive;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StrongVertices {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) b[i] = sc.nextInt();
+
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                a[i] = a[i] - b[i];
+                max = Math.max(max, a[i]);
+            }
+
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) if (a[i] == max) list.add(i + 1);
+
+            System.out.println(list.size());
+            for(int num : list) System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+}
