02b99e4b64673b36f4bc4df729008264d4cf058b
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-02-28 18:44:08 +0530

    We Are Reaching There!

---
+++ src/weeksix/MinimumReplacementsToSort.java
@@ -0,0 +1,25 @@
+package weeksix;
+
+public class MinimumReplacementsToSort {
+
+    public static long minimumReplacement(int[] nums) {
+        int curMax = nums[nums.length - 1];
+        long count = 0;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int num = nums[i];
+            if (num <= curMax) {
+                curMax = num;
+            } else {
+                if (num % curMax == 0) {
+                    count += (num / curMax - 1);
+                } else {
+                    int divide = num / curMax;
+                    count += divide;
+                    curMax = num / (divide + 1);
+                }
+            }
+        }
+        return count;
+    }
+}
