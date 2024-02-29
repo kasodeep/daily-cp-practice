50ca3d9a704783b237350cd8f2021882a7f56681
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-02-29 16:26:00 +0530

    Great day! Now New Goals New Level!

---
+++ src/daily/gfg/SumOfBitDifferences.java
@@ -0,0 +1,17 @@
+package daily.gfg;
+
+public class SumOfBitDifferences {
+
+    long sumBitDifferences(int[] arr, int n) {
+        long ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++)
+                if ((arr[j] & (1 << i)) != 0) count++;
+            ans += (long) count * (n - count) * 2;
+        }
+
+        return ans;
+    }
+}
