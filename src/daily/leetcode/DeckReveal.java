59060d6eaffbbc8a37aaba41861040d99aba9d9e
Kasodariya Deep <106021230+kasodeep@users.noreply.github.com>
2024-04-10 11:18:03 +0530

    Break And Back!

---
+++ src/daily/leetcode/DeckReveal.java
@@ -0,0 +1,28 @@
+package daily.leetcode;
+
+import java.util.Arrays;
+
+public class DeckReveal {
+
+    public static int[] deckRevealedIncreasing(int[] deck) {
+        int N = deck.length;
+        int[] result = new int[N];
+
+        boolean skip = false;
+        int indexInDeck = 0;
+        int indexInResult = 0;
+
+        Arrays.sort(deck);
+        while (indexInDeck < N) {
+            if (result[indexInResult] == 0) {
+                if (!skip) {
+                    result[indexInResult] = deck[indexInDeck];
+                    indexInDeck++;
+                }
+                skip = !skip;
+            }
+            indexInResult = (indexInResult + 1) % N;
+        }
+        return result;
+    }
+}
